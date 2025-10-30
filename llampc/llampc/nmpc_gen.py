34ace6c51e6818a37e285fde639ab0b89e57af7c
Henry Liao <hzl@andrew.cmu.edu>
2025-10-30 19:16:19 -0400

    finished run

--- llampc/llampc/nmpc_gen.py
+++ llampc/llampc/nmpc_gen.py
@@ -100,7 +100,7 @@ def create_ocp(model, params_car, steps, horizon):
     w_y = 1.0
     w_xe = 0
     w_ye = 0
-    w_steer = .05
+    w_steer = .01
     w_accel = 0.001
     w_jerk = .001
     w_steer_v = 0.005
