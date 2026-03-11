420d8fbfc4ae3cba2ceafdfa3a228cc844743f30
Henry Liao <hzl@andrew.cmu.edu>
2026-03-11 07:44:08 -0400

    ls

--- llampc/llampc/nmpc_gen.py
+++ llampc/llampc/nmpc_gen.py
@@ -106,14 +106,14 @@ def create_ocp(model, params_car, steps, horizon):
     # w_steer_v = 0
     
 
-    w_x = 1.0
-    w_y = 1.0
+    w_x = 2.0
+    w_y = 2.0
     w_xe = 0
     w_ye = 0
     w_steer = 0.03
     w_accel = 0.01
     w_jerk = .001
-    w_steer_v = 0.001
+    w_steer_v = 0.01
     Q_flat = [w_x, w_y, 0.0, 0.0, 0.0, 0.0]
     R_flat = [w_accel, w_steer]
     Rd_flat = [w_jerk, w_steer_v]
