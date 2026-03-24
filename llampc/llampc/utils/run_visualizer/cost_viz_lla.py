84a57d5d1bab21110caf0f9706ff748363f7724d
henry <hzl@andrew.cmu.edu>
2026-03-24 08:13:45 -0400

    track

--- llampc/llampc/utils/run_visualizer/cost_viz_lla.py
+++ llampc/llampc/utils/run_visualizer/cost_viz_lla.py
@@ -375,7 +375,7 @@ class GridSearchVisualizer:
 def main():
     dir_path = os.path.dirname(os.path.abspath(__file__))
     # Assuming 'all_batch_best_trajectories.npz' or similar is the output
-    visualizer = GridSearchVisualizer(os.path.join(dir_path, 'nshtrack.npz'), os.path.join(dir_path, 'all_batch_best_trajectories.npz'), 20)
+    visualizer = GridSearchVisualizer(os.path.join(dir_path, 'nshtrack.npz'), os.path.join(dir_path, 'lla_first.npz'), 20)
     visualizer.show()
 
 if __name__ == "__main__":
