72a38898e9056fdcd0b36db0dd97e1a3bae13ec4
Henry Liao <hzl@andrew.cmu.edu>
2026-05-17 23:17:14 -0400

    old stuff

--- llampc/llampc/nmpc_gen_pwm.py
+++ llampc/llampc/nmpc_gen_pwm.py
@@ -301,10 +301,10 @@ def create_ocp(model, params_car, steps, horizon):
     ocp.cost.cost_type = 'NONLINEAR_LS'
     ocp.cost.cost_type_e = 'NONLINEAR_LS'
 
-    w_x = 7.0
-    w_y = 7.0
-    w_xe = 1.0
-    w_ye = 1.0
+    w_x = 4.0
+    w_y = 4.0
+    w_xe = 0.1
+    w_ye = 0.1
     w_steer = 0.01
     w_accel = 40.0
     w_jerk = 0
