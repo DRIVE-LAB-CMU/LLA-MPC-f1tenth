93238f579f6877e4e6f49d4966f19b7351d6b9fc
Henry Liao <hzl@andrew.cmu.edu>
2026-05-18 02:35:08 -0400

    easier

--- llampc/llampc/utils/run_visualizer/viz_ref.py
+++ llampc/llampc/utils/run_visualizer/viz_ref.py
@@ -428,7 +428,7 @@ class StateVisualizer:
 def main():
     """Main entry point."""
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(dir_path, 'yawing.npz')
+    filepath = os.path.join(dir_path, 'crash.npz')
 
     ref_filepath = os.path.join(os.path.dirname(dir_path), 'tracks', 'mocap_square2.npz') 
     
