5ec81455b800ad3284d5d9ff784e28fcf3670a24
Henry Liao <hzl@andrew.cmu.edu>
2026-06-02 23:18:47 -0400

    fast

--- llampc/llampc/utils/run_visualizer/viz_ref.py
+++ llampc/llampc/utils/run_visualizer/viz_ref.py
@@ -428,7 +428,7 @@ class StateVisualizer:
 def main():
     """Main entry point."""
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(dir_path, 'xd.npz')
+    filepath = os.path.join(dir_path, 'stuff.npz')
 
     ref_filepath = os.path.join(os.path.dirname(dir_path), 'tracks', 'mocap_square2.npz') 
     
