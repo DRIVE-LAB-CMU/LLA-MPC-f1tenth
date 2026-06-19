f069bc2a21f04ee35fef43f02d2714b5c2d1cf97
Henry Liao <hzl@andrew.cmu.edu>
2026-06-19 19:40:55 -0400

    reset counter log

--- llampc/llampc/utils/run_visualizer/viz_ref.py
+++ llampc/llampc/utils/run_visualizer/viz_ref.py
@@ -1250,7 +1250,7 @@ class StateVisualizer:
 def main():
     """Main entry point."""
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(dir_path, 'cbfc.npz')
+    filepath = os.path.join(dir_path, 'cbfa.npz')
 
     ref_filepath = os.path.join(os.path.dirname(dir_path), 'tracks', 'mocap_square2slow.npz')
 
