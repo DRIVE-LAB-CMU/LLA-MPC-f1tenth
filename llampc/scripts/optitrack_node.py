5f1a14afc7ca1a10bda4f53acdb231a4ace39baa
Henry Liao <hzl@andrew.cmu.edu>
2026-06-29 22:16:47 -0400

    fix

--- llampc/scripts/optitrack_node.py
+++ llampc/scripts/optitrack_node.py
@@ -255,6 +255,8 @@ class OptitrackSubscriber(Node):
         # tcov[35] = 1e-2  # Vyaw variance
         # odom_msg.twist.covariance = tcov.tolist()
         # ==========================================================
+
+        
         # if len(self.position_history) >= 2:
         #     if self.position_history[-1] == self.position_history[-2] and \
         #         self.quaternion_history[-1] == self.quaternion_history[-2]:
