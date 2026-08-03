11da789aac8b20fc029f6a9b8136e35d65e19198
Henry Liao <hzl@andrew.cmu.edu>
2026-08-03 16:17:29 -0400

    publish to

--- llampc/scripts/llampc_fiala_fixed.py
+++ llampc/scripts/llampc_fiala_fixed.py
@@ -99,7 +99,7 @@ class MPCNode(Node):
 
         self.cmd_pub = self.create_publisher(
             AckermannDriveStamped,
-            '/mpc/drive',
+            '/drive',
             10
         )
 
